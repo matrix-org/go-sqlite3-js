327088cdef100282195dd31a52be70073e6723de
Kegan Dougal <kegan@matrix.org>
2020-03-25 17:49:27 +0000

    Refactor test rig into actual tests

--- sqlite3_test.go
+++ sqlite3_test.go
@@ -18,160 +18,158 @@ import (
 	"context"
 	"crypto/sha256"
 	"database/sql"
-	"log"
+	"fmt"
 	"testing"
 
 	_ "github.com/matrix-org/go-sqlite3-js"
 )
 
-func printResultMetadata(msg string, res sql.Result) {
-	if res == nil {
-		log.Printf("nil result")
-		return
+var i = 1
+
+func newDB(t *testing.T, schema string) *sql.DB {
+	var db *sql.DB
+	var err error
+	i += 1
+	if db, err = sql.Open("sqlite3_js", fmt.Sprintf("test-%d.db", i)); err != nil {
+		t.Fatalf("cannot open test.db: %s", err)
 	}
-	// the impl never returns an error on these functions
-	ra, err := res.RowsAffected()
+	_, err = db.Exec(schema)
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("cannot create schema: %s", err)
 	}
-	rowid, err := res.LastInsertId()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%s => rowid: %d rows affected: %d", msg, rowid, ra)
+	return db
 }
 
-func checkBlobSupport(db *sql.DB) {
-	log.Println("checkBlobSupport:")
-	blobStmt, err := db.Prepare("INSERT INTO bar(id, thing) values($1, $2)")
+func assertStored(t *testing.T, db *sql.DB, query string, wants []string) {
+	rows, err := db.Query(query)
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("assertStored: cannot run query: %s", err)
 	}
-	rawBytes := sha256.Sum256([]byte("hello world"))
-	_, err = blobStmt.Exec(44, rawBytes[:])
-	if err != nil {
-		log.Fatal(err)
+	defer rows.Close()
+	var gots []string
+	for rows.Next() {
+		var got string
+		if err := rows.Scan(&got); err != nil {
+			t.Fatalf("assertStored: failed to scan row: %s", err)
+		}
+		gots = append(gots, got)
 	}
-	blobSelectStmt, err := db.Prepare("SELECT thing FROM bar WHERE id = $1")
-	if err != nil {
-		log.Fatal(err)
+	if len(gots) != len(wants) {
+		t.Fatalf("assertStored: got %d results, want %d", len(gots), len(wants))
 	}
-	var bres []byte
-	if err := blobSelectStmt.QueryRow(44).Scan(&bres); err != nil {
-		log.Fatal(err)
-	}
-
-	if len(bres) != len(rawBytes) {
-		log.Fatalf("Mismatched lengths: got %d want %d", len(bres), len(rawBytes))
-	}
-	for i := range bres {
-		if bres[i] != rawBytes[i] {
-			log.Fatalf("Wrong value at pos %d/%d: got %d want %d", i, len(bres), bres[i], rawBytes[i])
+	for i := range wants {
+		if gots[i] != wants[i] {
+			t.Errorf("assertStored: result row %d got %s, want %s", i, gots[i], wants[i])
 		}
 	}
-	log.Println("OK: checked ", len(bres), " bytes")
 }
 
-func checkEmptyQuery(db *sql.DB) {
-	log.Println("checkEmptyQuery:")
+func TestEmptyQuery(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
 	stmt, err := db.Prepare("SELECT id, name FROM foo")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	// querying an empty table shouldn't produce an error
 	rows, err := stmt.Query()
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	rows.Close()
 }
 
-func checkEmptyQueryWithResults(db *sql.DB) {
+func TestEmptyStmtQueryWithResults(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
+
+	wantIDs := []int{11, 12, 13}
+	wantNames := []string{"Eleven", "Mike", "Dustin"}
+	for i := range wantIDs {
+		_, err := db.Exec("INSERT INTO foo VALUES(?,?)", wantIDs[i], wantNames[i])
+		if err != nil {
+			t.Fatalf("Insert failed: %s", err)
+		}
+	}
+
 	stmt, err := db.Prepare("SELECT id, name FROM foo")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	rows, err := stmt.Query()
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	defer rows.Close()
-
+	i := 0
 	for rows.Next() {
 		var id int
 		var name string
 		err := rows.Scan(&id, &name)
 		if err != nil {
-			log.Fatal(err)
+			t.Fatalf("Scan failed: %s", err)
 		}
-		log.Printf("Empty db.Query() got row: %d, %s", id, name)
+		if id != wantIDs[i] {
+			t.Errorf("Row %d: got ID %d, want %d", i, id, wantIDs[i])
+		}
+		if name != wantNames[i] {
+			t.Errorf("Row %d: got name %s, want %s", i, name, wantNames[i])
+		}
+		i++
+	}
+	if len(wantIDs) != i {
+		t.Errorf("Mismatched number of returned rows: %d != %d", len(wantIDs), i)
 	}
 }
 
-func checkErrNoRows(db *sql.DB) {
-	log.Println("checkErrNoRows:")
+func TestErrNoRows(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
 	stmt, err := db.Prepare("SELECT id, name FROM foo")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	// query row context should return ErrNoRows
 	var a int64
 	var b string
 	err = stmt.QueryRowContext(context.Background()).Scan(&a, &b)
 	if err != sql.ErrNoRows {
-		log.Fatalf("Expected sql.ErrNoRows to QueryRowContext, got %s", err)
-	} else {
-		log.Println("Returns sql.ErrNoRows ok")
+		t.Fatalf("Expected sql.ErrNoRows to QueryRowContext, got %s", err)
 	}
 }
 
-func checkMultipleConnSupport() {
+func TestMultipleConnSupport(t *testing.T) {
 	// We check this by doing multiple txns at once. If the same conn is used,
 	// we'll error out with:
 	//    sql.js: cannot start a transaction within a transaction
-	log.Println("checkMultipleConnSupport:")
-	var db *sql.DB
-	var err error
-	// Dendrite only does this once, then calls a bunch of stuff
-	if db, err = sql.Open("sqlite3_js", "file:test2.db"); err != nil {
-		log.Fatal(err)
-	}
 
+	// Dendrite only does this once, then calls a bunch of stuff
+	db := newDB(t, "CREATE TABLE foo(id INTEGER)")
 	tx, err := db.Begin()
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	_, err = tx.Exec("CREATE TABLE foo(id INTEGER)")
+	_, err = tx.Exec("CREATE TABLE bar(id INTEGER)")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("tx1 exec failed: %s", err)
 	}
+	// begin a 2nd txn without closing the 1st
 	tx2, err := db.Begin()
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	_, err = tx2.Exec("CREATE TABLE bar(id INTEGER)")
+	_, err = tx2.Exec("CREATE TABLE baz(id INTEGER)")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("tx2 exec failed: %s", err)
 	}
 	if err = tx2.Commit(); err != nil {
-		log.Fatal(err)
+		t.Fatalf("tx2 commit failed: %s", err)
 	}
 	if err = tx.Commit(); err != nil {
-		log.Fatal(err)
+		t.Fatalf("tx1 commit failed: %s", err)
 	}
 }
 
 func TestBlobSupport(t *testing.T) {
-	var db *sql.DB
-	var err error
-	if db, err = sql.Open("sqlite3_js", "test.db"); err != nil {
-		t.Fatal(err)
-	}
-	_, err = db.Exec("CREATE TABLE bar(id INTEGER, thing BLOB); create table foo(id INTEGER PRIMARY KEY, name string)")
-	if err != nil {
-		t.Fatal(err)
-	}
-	blobStmt, err := db.Prepare("INSERT INTO bar(id, thing) values($1, $2)")
+	db := newDB(t, "create table blobs(id INTEGER, thing BLOB)")
+	blobStmt, err := db.Prepare("INSERT INTO blobs(id, thing) values($1, $2)")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -180,7 +178,7 @@ func TestBlobSupport(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	blobSelectStmt, err := db.Prepare("SELECT thing FROM bar WHERE id = $1")
+	blobSelectStmt, err := db.Prepare("SELECT thing FROM blobs WHERE id = $1")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -200,115 +198,170 @@ func TestBlobSupport(t *testing.T) {
 	t.Log("OK: checked ", len(bres), " bytes")
 }
 
-func mainx() {
-	log.Printf("Opening sqlite3_js driver...")
-	var db *sql.DB
-	var err error
-	if db, err = sql.Open("sqlite3_js", "test.db"); err != nil {
-		log.Fatal(err)
-	}
-
-	// checks multiple queries can go in a single Exec call. We don't support params in this form.
-	_, err = db.Exec("CREATE TABLE bar(id INTEGER, thing BLOB); create table foo(id INTEGER PRIMARY KEY, name string)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = db.Exec("CREATE TABLE bar2(id INTEGER, thing BLOB); create table foo(id INTEGER PRIMARY KEY, name string)")
-	if err == nil {
-		log.Fatal("Expected error overwriting table 'foo'")
-	}
-
-	checkBlobSupport(db)
-
-	checkEmptyQuery(db)
-
-	checkErrNoRows(db)
-
+func TestInsertNull(t *testing.T) {
+	db := newDB(t, "create table bar(id INTEGER PRIMARY KEY, name string)")
 	res, err := db.Exec("insert into bar values(9001, NULL)")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	printResultMetadata("After insert on db", res)
+	if ra, _ := res.RowsAffected(); ra != 1 {
+		t.Fatalf("expected 1 row affected, got %d", ra)
+	}
+}
 
-	res, err = db.Exec("insert into foo values(42, 'meaning of life')")
+func TestInsertPrimaryKeyConflict(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
+	_, err := db.Exec("insert into foo values(42, 'meaning of life')")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	printResultMetadata("After insert on db", res)
-
-	res, err = db.Exec("insert into foo values(42, 'meaning of life')")
+	_, err = db.Exec("insert into foo values(42, 'meaning of life')")
 	if err == nil {
-		log.Fatal("Expected err returned from primary key conflict INSERT but got nil")
+		t.Fatal("Expected error, got nil")
 	}
+}
 
-	res, err = db.Exec("insert into foo values(43, 'meaning of life')")
+func TestUpdate(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
+	_, err := db.Exec("insert into foo values(42, 'meaning of life')")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("failed to insert: %s", err)
 	}
+	assertStored(t, db, "SELECT name FROM foo", []string{"meaning of life"})
 
-	res, err = db.Exec("UPDATE foo SET name='mol' WHERE name='meaning of life'")
+	_, err = db.Exec("UPDATE foo SET name='mol' WHERE name='meaning of life'")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("failed to update: %s", err)
 	}
-	printResultMetadata("After updating 2 rows", res)
 
-	_, err = db.Exec("insert into foo values(?, ?)", 31337, "so leet")
+	assertStored(t, db, "SELECT name FROM foo", []string{"mol"})
+}
+
+func TestParameterisedInsert(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
+	_, err := db.Exec("insert into foo values(?, ?)", 31337, "so leet")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
+	assertStored(t, db, "SELECT name FROM foo", []string{"so leet"})
+}
 
+func TestParameterisedStmtExec(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
 	var stmt *sql.Stmt
+	var err error
 	if stmt, err = db.Prepare("insert into foo values(?, ?)"); err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	res, err = stmt.Exec(12345678, "monotonic")
-	printResultMetadata("After insert on stmt", res)
+	_, err = stmt.Exec(12345678, "monotonic")
+	if err != nil {
+		t.Fatalf("Failed exec: %s", err)
+	}
+	assertStored(t, db, "SELECT name FROM foo", []string{"monotonic"})
+}
 
+func TestCommit(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
 	var txn *sql.Tx
+	var stmt *sql.Stmt
+	var err error
 	if txn, err = db.Begin(); err != nil {
-		log.Fatal(err)
+		t.Fatalf("begin failed: %s", err)
 	}
 	if stmt, err = db.Prepare("insert into foo values(?, ?)"); err != nil {
-		log.Fatal(err)
+		t.Fatalf("prepare failed: %s", err)
 	}
 	stmt = txn.Stmt(stmt)
-	stmt.Exec(666, "not happening")
-	txn.Rollback()
+	_, err = stmt.Exec(999, "happening")
+	if err != nil {
+		t.Fatalf("exec failed: %s", err)
+	}
+	if err = txn.Commit(); err != nil {
+		t.Fatalf("Commit failed: %s", err)
+	}
+	assertStored(t, db, "SELECT name FROM foo", []string{"happening"})
+}
 
+func TestRollback(t *testing.T) {
+	t.Skip() // txn support disabled
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
+	var txn *sql.Tx
+	var stmt *sql.Stmt
+	var err error
 	if txn, err = db.Begin(); err != nil {
-		log.Fatal(err)
+		t.Fatalf("begin failed: %s", err)
 	}
 	if stmt, err = db.Prepare("insert into foo values(?, ?)"); err != nil {
-		log.Fatal(err)
+		t.Fatalf("prepare failed: %s", err)
 	}
 	stmt = txn.Stmt(stmt)
-	res, err = stmt.Exec(999, "happening")
-	printResultMetadata("After insert on stmt in txn", res)
-	txn.Commit()
+	_, err = stmt.Exec(666, "not happening")
+	if err != nil {
+		t.Fatalf("exec failed: %s", err)
+	}
+	if err = txn.Rollback(); err != nil {
+		t.Fatalf("rollback failed: %s", err)
+	}
+	assertStored(t, db, "SELECT name FROM foo", []string{})
+}
 
-	if stmt, err = db.Prepare("select * from foo"); err != nil {
-		log.Fatal(err)
+func TestStarSelectSingle(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
+	wantID := 11
+	wantName := "Eleven"
+	_, err := db.Exec("INSERT INTO foo VALUES(?,?)", wantID, wantName)
+	if err != nil {
+		t.Fatalf("Insert failed: %s", err)
+	}
+	stmt, err := db.Prepare("select * from foo")
+	if err != nil {
+		t.Fatal(err)
 	}
 	var id int
 	var name string
 	stmt.QueryRow().Scan(&id, &name)
-	log.Printf("Got first row: %d, %s", id, name)
 	stmt.Close()
+	if id != wantID {
+		t.Errorf("ID: got %d want %d", id, wantID)
+	}
+	if name != wantName {
+		t.Errorf("Name got %s want %s", name, wantName)
+	}
+}
+
+func TestStarSelectMulti(t *testing.T) {
+	db := newDB(t, "create table foo(id INTEGER PRIMARY KEY, name string)")
+	wantIDs := []int{11, 12, 13}
+	wantNames := []string{"Eleven", "Mike", "Dustin"}
+	for i := range wantIDs {
+		_, err := db.Exec("INSERT INTO foo VALUES(?,?)", wantIDs[i], wantNames[i])
+		if err != nil {
+			t.Fatalf("Insert failed: %s", err)
+		}
+	}
 
 	rows, err := db.Query("select * from foo")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
+	defer rows.Close()
+	i := 0
 	for rows.Next() {
+		var id int
+		var name string
 		err := rows.Scan(&id, &name)
 		if err != nil {
-			log.Fatal(err)
+			t.Fatalf("Scan failed: %s", err)
 		}
-		log.Printf("Got row: %d, %s", id, name)
+		if id != wantIDs[i] {
+			t.Errorf("Row %d: got ID %d, want %d", i, id, wantIDs[i])
+		}
+		if name != wantNames[i] {
+			t.Errorf("Row %d: got name %s, want %s", i, name, wantNames[i])
+		}
+		i++
 	}
-	rows.Close()
-
-	checkEmptyQueryWithResults(db)
-
-	checkMultipleConnSupport()
+	if len(wantIDs) != i {
+		t.Errorf("Mismatched number of returned rows: %d != %d", len(wantIDs), i)
+	}
 }
--- tests/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/matrix-org/go-sqlite3-js/tests
-
-replace github.com/matrix-org/go-sqlite3-js => ../
-
-go 1.13
